be68113b5a2d81b1e62bb96df3ea7a9c86d99a0f
KushGoyner <kushgoynerkg@gmail.com>
2025-02-02 12:31:02 +0530

    "Fixed Cookie Not saving"

--- server/server.js
+++ server/server.js
@@ -8,16 +8,18 @@ import cors from "cors"
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN |"http://localhost:3000" ,
+//   credentials: true,
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true , limit:"16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 app.use(morgan('dev'));
+
+
 app.use(cors(
   {
     origin: "http://localhost:3000", // Replace with your frontend URL
